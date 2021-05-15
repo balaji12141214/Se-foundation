d3f220c9fde2fa6dc72fb563b7922067d296d15e
balaji <balaji236161@gmail.com>
2021-05-15 13:47:15 +0530

    Runs even fails with internet connection

--- satyaeducare/src/test/java/pages/SeFoundationSiteOpeningUp.java
+++ satyaeducare/src/test/java/pages/SeFoundationSiteOpeningUp.java
@@ -1,5 +1,7 @@
 package pages;
 
+import static org.testng.Assert.fail;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
@@ -12,73 +14,70 @@ public class SeFoundationSiteOpeningUp extends TestBase {
 
 	static int time = 3;
 	static int executedCount; // how many times code executed
-	static boolean noIssue = true;
 
 	TestBase util = new TestBase();
 	WebDriver driver;
 	WebElements elements = new WebElements();
 
-	@Test(invocationCount = /*100*/ 2)
+	@Test(invocationCount = 100)
 	public void initialize() {
 
 		try {
-			if (noIssue) {
-				util.displayMessage("instantiating driver");
-				driver = util.instantiateDriver();
 
-				util.displayMessage("maximizing browser window");
-				driver.manage().window().maximize();
+			util.displayMessage("instantiating driver");
+			driver = util.instantiateDriver();
 
-				util.displayMessage("waiting to window maximize");
-				util.simply(time);
+			util.displayMessage("maximizing browser window");
+			driver.manage().window().maximize();
 
-				util.displayMessage("navigating to google url");
-				driver.get("https://google.com");
+			util.displayMessage("waiting to window maximize");
+			util.simply(time);
 
-				util.displayMessage("waiting for load google page");
-				util.simply(time);
+			util.displayMessage("navigating to google url");
+			driver.get("https://google.com");
 
-				util.displayMessage("entering 'se-foundation' text in search place");
-				driver.findElements(elements.googleSearchBar).get(0).sendKeys("se-foundation.com");
+			util.displayMessage("waiting for load google page");
+			util.simply(time);
 
-				util.displayMessage("hitting 'Enter' button after giving 'se-foundation' text");
-				driver.findElements(elements.googleSearchBar).get(0).sendKeys(Keys.ENTER);
-				util.simply(time);
+			util.displayMessage("entering 'se-foundation' text in search place");
+			driver.findElements(elements.googleSearchBar).get(0).sendKeys("se-foundation.com");
 
-				util.displayMessage("clicking on satya educare from shown results");
-				driver.findElement(elements.satyaEducareText).click();
+			util.displayMessage("hitting 'Enter' button after giving 'se-foundation' text");
+			driver.findElements(elements.googleSearchBar).get(0).sendKeys(Keys.ENTER);
+			util.simply(time);
 
-				util.displayMessage("getting current url and storing in 'url' named variable");
-				String url = driver.getCurrentUrl();
+			util.displayMessage("clicking on satya educare from shown results");
+			driver.findElement(elements.satyaEducareText).click();
 
-				util.displayMessage("Asserting page is navigated to the 'se-foundation' or not");
-				Assert.assertTrue(url.contains("se-foundation.com"));
-				util.simply(time - 1);
+			util.displayMessage("getting current url and storing in 'url' named variable");
+			String url = driver.getCurrentUrl();
 
-				util.displayMessage("Below Line display How many times successfully completed");
-				System.out.println(executedCount = executedCount + 1);
+			util.displayMessage("Asserting page is navigated to the 'se-foundation' or not");
+			Assert.assertTrue(url.contains("se-foundation.com"));
+			util.simply(time - 1);
 
-				util.displayMessage("wait for 1 minutes, for next invocation");
-				try {
-					driver.close();
-					util.waitTime();
-				} catch (InterruptedException e) {
+			util.displayMessage("Below Line display How many times successfully completed");
+			System.out.println(executedCount = executedCount + 1);
 
-				}
+			util.displayMessage("wait for 1 minutes, for next invocation");
+			try {
+				driver.close();
+				util.waitTime();
+			} catch (InterruptedException e) {
 
 			}
-			if (!noIssue) {
-				Assert.fail();
-			}
-		} catch (Exception e) {
+
+		} catch (
+
+		Exception e) {
 			util.displayMessage("Found Issue");
-			noIssue = false;
-			Assert.fail();
+			driver.close();
+			fail();
 
 		} catch (AssertionError er) {
 			util.displayMessage("Found Issue");
-			noIssue = false;
-			Assert.fail();
+			driver.close();
+			fail();
 
 		}
 	}
